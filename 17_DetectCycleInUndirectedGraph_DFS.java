70d7ba7bb135745141a96cbbdaadab344ee60023
Jha-RaushanKumar <jha.rau@northeastern.edu>
2023-03-09 15:34:11 -0500

    Create 17_DetectCycleInUndirectedGraph_DFS.java

---
+++ 17_DetectCycleInUndirectedGraph_DFS.java
@@ -0,0 +1,31 @@
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        
+        boolean vis[] = new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                vis[i] = true;
+                if(dfs(i,-1,vis,adj) == true){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean dfs(int node,int parent, boolean vis[], ArrayList<ArrayList<Integer>> adj){
+        vis[node] = true;
+        for(int it : adj.get(node)){
+            if(!vis[it]){
+                if(dfs(it,node,vis,adj) == true){
+                    return true;
+                }
+            }
+            else if(it != parent){
+                return true;
+            }
+        }
+        return false;
+    }
+}
